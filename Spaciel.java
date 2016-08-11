510d8321ac433ac365e711ac7f84de1b5dc36572
suganyaramasamy <suganya.rcse@ksrce.ac.in>
2016-08-11 11:23:10 +0530

    Create Spaciel.java

---
+++ Spaciel.java
@@ -0,0 +1,15 @@
+package GuviDemo;
+
+public class ExArray {
+
+public static void main(String[] args) {
+	String str=" ";
+	int n=5;
+	for(int i=0;i<n;i++)
+	{
+		str+="#";
+
+	System.out.print(" " + str + " " );
+	}
+}
+}

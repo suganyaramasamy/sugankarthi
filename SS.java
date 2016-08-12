57e23e3e39af432f1a8fb35e646e3bbcefd7ae82
suganyaramasamy <suganya.rcse@ksrce.ac.in>
2016-08-12 22:33:31 +0530

    Create SS.java

---
+++ SS.java
@@ -0,0 +1,14 @@
+package guvi;
+
+public class rEVER {
+
+	public static void main(String[] args) {
+		String str[] = "He is the one".split(" ");
+	    String finalStr="";
+	        for(int i = str.length-1; i>= 0 ;i--){
+	            finalStr += str[i]+" ";
+	        }
+	        System.out.println(finalStr);
+	}
+
+}

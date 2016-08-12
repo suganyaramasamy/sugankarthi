4f435087b39b1bc27fbf5c1389dfc1a66d4dcac7
suganyaramasamy <suganya.rcse@ksrce.ac.in>
2016-08-12 22:01:52 +0530

    Create Addition.java

---
+++ Addition.java
@@ -0,0 +1,15 @@
+package guvi;
+
+public class Aclass {
+
+	 public static void main(String[] args) {
+int a[]={1,2,3};
+for(int i=0;i<a[i];i++)
+{
+	a[i+2]=a[i]+a[i+1];
+	System.out.println(a[i+2]);
+	
+	}
+
+}
+}

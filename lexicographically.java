a52ea708ee75ef17ea9cee7c13fad6d25e6827f5
suganyaramasamy <suganya.rcse@ksrce.ac.in>
2016-08-12 21:35:53 +0530

    Create lexicographically.java

---
+++ lexicographically.java
@@ -0,0 +1,12 @@
+package guvi;
+
+public class Lexico {
+
+	public static void main(String[] args) {
+
+		String str="abcdef";
+		String str1="abcdefg";
+		System.out.println(str.compareTo(str1));
+	}
+
+}
